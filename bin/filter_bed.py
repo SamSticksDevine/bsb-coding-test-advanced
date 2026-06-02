9bb89cc08cb736a7b4cd527e70c0dbb0a1e9791d
Sam Devine <samh18@sky.com>
2026-06-02 12:41:27 +0100

    finalised the python scripts for the nextflow run

--- bin/filter_bed.py
+++ bin/filter_bed.py
@@ -45,12 +45,11 @@ def parse_args():
 
     return parser.parse_args()
 
-
 if __name__ == "__main__":
-
+        
     args = parse_args()
 
     with open(args.input_bed) as infile, open(args.output_bed, "w") as outfile:
 
         for line in filter_records(infile, args.min_mapq):
-            outfile.write(line) 
+            outfile.write(line)
